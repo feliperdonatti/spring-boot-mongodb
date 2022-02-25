b28d5023e608ebae3da44b6ef98013e34f559126
feliperdonatti <feliperdonatti@gmail.com>
2022-02-25 13:56:54 -0300

    Criacao de UserDto

--- src/main/java/com/feliperdonatti/workshopmongo/resource/UserResource.java
+++ src/main/java/com/feliperdonatti/workshopmongo/resource/UserResource.java
@@ -1,8 +1,8 @@
 package com.feliperdonatti.workshopmongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.feliperdonatti.workshopmongo.domain.User;
+import com.feliperdonatti.workshopmongo.dto.UserDto;
 import com.feliperdonatti.workshopmongo.services.UserService;
 
 @RestController
@@ -21,10 +22,12 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDto>> findAll(){
 		List<User> list  = service.findAll();
 		
-		return ResponseEntity.ok().body(list);
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 
 	}
 
