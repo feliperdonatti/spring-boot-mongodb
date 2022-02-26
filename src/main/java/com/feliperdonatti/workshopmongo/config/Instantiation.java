1839122a679421f1ee1f4e4d88aafc4e335d8ca3
feliperdonatti <feliperdonatti@gmail.com>
2022-02-25 22:24:55 -0300

    AuthorDto

--- src/main/java/com/feliperdonatti/workshopmongo/config/Instantiation.java
+++ src/main/java/com/feliperdonatti/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.feliperdonatti.workshopmongo.domain.Post;
 import com.feliperdonatti.workshopmongo.domain.User;
+import com.feliperdonatti.workshopmongo.dto.AuthorDto;
 import com.feliperdonatti.workshopmongo.repository.PostRepository;
 import com.feliperdonatti.workshopmongo.repository.UserRepository;
 
@@ -31,14 +32,16 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-		Post post1 = new Post(null, sdf.parse("2022-01-29"), "Partiu Viagem", "Vou viajar para São Paulo! Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("2021-12-18"), "Bom dia", "Acordei Feliz Hoje", maria);
+		Post post1 = new Post(null, sdf.parse("2022-01-29"), "Partiu Viagem", "Vou viajar para São Paulo! Abraços", new AuthorDto(maria));
+		Post post2 = new Post(null, sdf.parse("2021-12-18"), "Bom dia", "Acordei Feliz Hoje", new AuthorDto(maria));
 		
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
--- src/main/java/com/feliperdonatti/workshopmongo/domain/Post.java
+++ src/main/java/com/feliperdonatti/workshopmongo/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.feliperdonatti.workshopmongo.dto.AuthorDto;
+
 @Document
 public class Post implements Serializable {
 
@@ -16,13 +18,13 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDto author;
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDto author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -63,11 +65,11 @@ public class Post implements Serializable {
 	}
 	
 
-	public User getAuthor() {
+	public AuthorDto getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDto author) {
 		this.author = author;
 	}
 
