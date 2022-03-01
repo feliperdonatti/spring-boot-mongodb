4f7aadc08f95e0c3d1f6e49b3045a4839d1fa29a
feliperdonatti <feliperdonatti@gmail.com>
2022-03-01 09:50:28 -0300

    endpoint retorna posts por usuario

--- src/main/java/com/feliperdonatti/workshopmongo/resource/UserResource.java
+++ src/main/java/com/feliperdonatti/workshopmongo/resource/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.feliperdonatti.workshopmongo.domain.Post;
 import com.feliperdonatti.workshopmongo.domain.User;
 import com.feliperdonatti.workshopmongo.dto.UserDto;
 import com.feliperdonatti.workshopmongo.services.UserService;
@@ -69,6 +70,14 @@ public class UserResource {
 		obj = service.update(obj);
 				
 		return ResponseEntity.noContent().build();
+
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj  = service.findById(id);
+				
+		return ResponseEntity.ok().body(obj.getPosts());
 
 	}
 
