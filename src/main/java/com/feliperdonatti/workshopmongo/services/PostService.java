172f692cd0882642a1c42ba846c08b3fdb00b9a4
feliperdonatti <feliperdonatti@gmail.com>
2022-03-01 10:15:27 -0300

    posts by id

--- src/main/java/com/feliperdonatti/workshopmongo/services/PostService.java
+++ src/main/java/com/feliperdonatti/workshopmongo/services/PostService.java
@@ -1,16 +1,12 @@
 package com.feliperdonatti.workshopmongo.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.feliperdonatti.workshopmongo.domain.Post;
-import com.feliperdonatti.workshopmongo.domain.User;
-import com.feliperdonatti.workshopmongo.dto.UserDto;
 import com.feliperdonatti.workshopmongo.repository.PostRepository;
-import com.feliperdonatti.workshopmongo.repository.UserRepository;
 import com.feliperdonatti.workshopmongo.services.exception.ObjectNotFoundException;
 
 
@@ -20,9 +16,6 @@ public class PostService {
 	@Autowired
 	private PostRepository repository;
 	
-	public List<Post> findAll(){
-		return repository.findAll();
-	}
 	
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
@@ -30,30 +23,6 @@ public class PostService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Post.class.getName()));
 	}
 	
-	public Post insert(Post post) {
-		return repository.save(post);
-	}
 	
-	public void delete(String id) {
-		findById(id);
-		repository.deleteById(id);
-	}
-	
-	public Post update(Post obj) {
-		Post newObj = findById(obj.getId());
-		//updateData(newObj, obj);
-		return repository.save(newObj);
-	}
-	/*
-	private void updateData(Post newObj, Post obj) {
-		newObj.setName(obj.getName());
-		newObj.setEmail(obj.getEmail());
-		
-	}
-	*/
-	public User fromDto(UserDto userDto) {
-		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
-		
-	}
 
 }
